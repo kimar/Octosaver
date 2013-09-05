76f1a644b04f695f29d8c02382520eb75a7e05b5
Marcus Kida <mail@marcuskida.de>
2013-09-05 20:05:46 +0200

    Initial Commit

---
+++ Octosaver/OctosaverView.h
@@ -0,0 +1,13 @@
+//
+//  OctosaverView.h
+//  Octosaver
+//
+//  Created by Marcus Kida on 05.09.13.
+//  Copyright (c) 2013 Marcus Kida. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface OctosaverView : ScreenSaverView
+
+@end
---
+++ Octosaver/OctosaverView.m
@@ -0,0 +1,52 @@
+//
+//  OctosaverView.m
+//  Octosaver
+//
+//  Created by Marcus Kida on 05.09.13.
+//  Copyright (c) 2013 Marcus Kida. All rights reserved.
+//
+
+#import "OctosaverView.h"
+
+@implementation OctosaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
